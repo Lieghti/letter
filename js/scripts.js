019864a8a45132912a9e5312b60fdfe37773757b
Lieghti Sharp and Robin Sola <lieghti@>
2017-10-11 20:44:24 -0700

    add form and js to create letter with submit button.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".blanks form").submit(function(event){
+  var name1Input = $("input#name1").val();
+
+
+  $(".name1").text(name1Input);
+  //$(".blanks").hide();
+  $("#letter").show();
+
+  event.preventDefault();
+  });
+});
